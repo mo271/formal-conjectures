63cf47e3355fa4462e7b67cd72d6b5a9a5ddb5e4
Moritz Firsching <firsching@google.com>
2025-07-18 17:37:51 +0200

    add statement on index shift

--- FormalConjectures/ErdosProblems/251.lean
+++ FormalConjectures/ErdosProblems/251.lean
@@ -25,7 +25,9 @@ import FormalConjectures.Util.ProblemImports
 /--
 Is
 $$\sum_{n} \frac{p_n}{2^n}$$
-irrational? Here $p_n$ is the $n$-th prime.
+irrational? Here $p_n$ is the $n$-th prime. With the convention $p_1 = 2$, the sum in the
+formal statement is half the sum in the informal statement, which doesn't influence the
+irrationality.
 -/
 @[category research open, AMS 11]
 theorem erdos_251 : Irrational (∑' n : ℕ, (Nat.nth Nat.Prime n) / (2 ^ n)) ↔ answer(sorry) := by
