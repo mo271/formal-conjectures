68b513e05d4cba2391be5ed25ea918794e2f5f2d
Moritz Firsching <firsching@google.com>
2025-09-02 07:38:54 +0200

    more docstrings

--- FormalConjectures/ForMathlib/Combinatorics/SimpleGraph/Coloring.lean
+++ FormalConjectures/ForMathlib/Combinatorics/SimpleGraph/Coloring.lean
@@ -21,18 +21,34 @@ variable {V : Type u} (G : SimpleGraph V)
 
 namespace SimpleGraph
 
+/--
+A set of edges is critical  if deleting them reduces the chromatic number.
+-/
 def IsCriticalEdges (edges : Set (Sym2 V)) : Prop :=
   (G.deleteEdges edges).chromaticNumber < G.chromaticNumber
 
+/--
+An edge is critical if deleting it reduces the chromatic number.
+-/
 def IsCriticalEdge (e : Sym2 V) : Prop := G.IsCriticalEdges ({e} : Set (Sym2 V))
 
 variable {G}
 
+/--
+A set of vertices is critical if deleting them reduces the chromatic number.
+-/
 def Subgraph.IsCritialVerts (verts : Set V) (G' : G.Subgraph) : Prop :=
   (G'.deleteVerts verts).coe.chromaticNumber < G'.coe.chromaticNumber
 
+/--
+A vertex is critical if deleting it reduces the chromatic number.
+-/
 def Subgraph.IsCritialVertex (v : V) (G' : G.Subgraph) : Prop := G'.IsCritialVerts {v}
 
 variable (G)
 
+/--
+A graph `G` is `k`-critical (or vertex-critical) if its chromatic number is `k`,
+and deleting any single vertex reduces the chromatic number.
+-/
 def IsCritical (k : ℕ) : Prop := G.chromaticNumber = k ∧ ∀ v, (⊤ : G.Subgraph).IsCritialVertex v
