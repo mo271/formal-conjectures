0ec9ed239ac1f485acea13bc61e296f8e161af25
Moritz Firsching <firsching@google.com>
2025-09-06 12:22:50 +0000

    add docstrings and some tests

--- FormalConjectures/ErdosProblems/74.lean
+++ FormalConjectures/ErdosProblems/74.lean
@@ -33,16 +33,68 @@ open Erdos74
 universe u
 variable {V : Type u}
 
-
-
+/--
+For a given subgraph `A`, this is the set of all numbers `k` such that `A` can be made
+bipartite by deleting `k` edges.
+-/
 def SimpleGraph.edgeDistancesToBipartite {G : SimpleGraph V} (A : G.Subgraph) : Set ℕ :=
   { (E.ncard) | (E : Set (Sym2 V)) (_ : IsBipartite (A.deleteEdges E).coe)}
 
+/--
+The set of edge distances to a bipartite graph is always non-empty because deleting all edges
+from a graph makes it bipartite.
+-/
+@[category test, AMS 5]
+theorem SimpleGraph.edgeDistancesToBipartite_nonempty {G : SimpleGraph V} (A : G.Subgraph) :
+    SimpleGraph.edgeDistancesToBipartite A |>.Nonempty := by
+  dsimp only [edgeDistancesToBipartite,Set.nonempty_def]
+  refine ⟨_, Set.univ, ?_, rfl⟩
+  use fun v => 0
+  simp
+
+/--
+The minimum number of edges that must be deleted from a subgraph `A` to make it bipartite.
+-/
 noncomputable def SimpleGraph.minEdgeDistToBipartite {G : SimpleGraph V} (A : G.Subgraph) : ℕ :=
   sInf <| SimpleGraph.edgeDistancesToBipartite A
 
+/--
+For a graph `G` and a number `n`, this is the set of `minEdgeDistToBipartite A` for all
+induced subgraphs `A` of `G` on `n` vertices.
+-/
 def SimpleGraph.subgraphEdgeDistsToBipartite (G : SimpleGraph V) (n : ℕ) : Set ℕ :=
-  { (SimpleGraph.minEdgeDistToBipartite A) | (A : Subgraph G) (_ : A.verts.ncard = n) }
+  { (SimpleGraph.minEdgeDistToBipartite A) |
+    (A : Subgraph G) (_ : A.verts.ncard = n) (_ : A.verts.Finite) }
+
+/--
+The set of minimum edge distances to bipartite for subgraphs of size `n` is bounded above.
+A graph on `n` vertices has at most `n choose 2` edges, and deleting all of them
+makes the graph bipartite, providing a straightforward upper bound.
+-/
+@[category test, AMS 5]
+theorem SimpleGraph.subgraphEdgeDistsToBipartite_bddAbove (G : SimpleGraph V) (n : ℕ) :
+    BddAbove (SimpleGraph.subgraphEdgeDistsToBipartite G n) := by
+  use n.choose 2
+  simp only [upperBounds, Set.mem_setOf_eq, SimpleGraph.subgraphEdgeDistsToBipartite,
+    SimpleGraph.minEdgeDistToBipartite, SimpleGraph.edgeDistancesToBipartite]
+  intro m h
+  replace ⟨A, ⟨hn, h_fin, h⟩⟩ := h
+  rw [← h]
+  have : A.edgeSet.ncard ≤ n.choose 2 := by
+    rw [← hn]
+    have := h_fin.fintype
+    have := Fintype.ofFinite ↑A.coe.edgeSet
+    convert (A.coe).card_edgeFinset_le_card_choose_two
+  refine le_trans ?_ this
+  apply Nat.sInf_le
+  simp only [Subgraph.deleteEdges_verts, exists_prop, Set.mem_setOf_eq]
+  use A.edgeSet
+  constructor
+  · use 0
+    rintro ⟨v, hv⟩ ⟨w, hw⟩ ⟨_, hvw⟩
+    aesop
+  · rfl
+
 
 /--
 For a given Graph $G$, define a number such that any subgraph of G
@@ -55,15 +107,6 @@ This is Definition 3.1 in [EHS82].
 noncomputable def SimpleGraph.maxSubgraphEdgeDistToBipartite
     (G : SimpleGraph V) (n : ℕ) : ℕ := sSup <| SimpleGraph.subgraphEdgeDistsToBipartite G n
 
-
-/--
-Deleting all edges will always be sufficient to make the graph bipartite.
--/
-@[category undergraduate, AMS 5]
-lemma max_subgraph_edge_dist_bipartite_le (G : SimpleGraph V) (n : ℕ) :
-    G.maxSubgraphEdgeDistToBipartite n ≤ n.choose 2 := by
-  sorry
-
 /--
 Let $f(n)\to \infty$ possibly very slowly.
 Is there a graph of infinite chromatic number such that every finite subgraph on $n$
@@ -72,7 +115,7 @@ vertices can be made bipartite by deleting at most $f(n)$ edges?
 @[category research open, AMS 5]
 theorem erdos_74 : (∀ f : ℕ → ℕ, Tendsto f atTop atTop →
     (∃ G : SimpleGraph V, G.chromaticNumber = ⊤ ∧
-    ∀ n, G.max_subgraph_bipartite_edge_deletion_num n ≤ f n)) ↔ answer(sorry):= by
+    ∀ n, G.maxSubgraphEdgeDistToBipartite n ≤ f n)) ↔ answer(sorry):= by
   sorry
 
 /--
@@ -81,7 +124,7 @@ vertices can be made bipartite by deleting at most $\sqrt{n}$ edges?
 -/
 @[category research open, AMS 5]
 theorem erdos_74.variants.sqrt : (∃ G : SimpleGraph V, G.chromaticNumber = ⊤ ∧
-    ∀ n, G.max_subgraph_bipartite_edge_deletion_num n ≤ (n : ℝ).sqrt) ↔ answer(sorry):= by
+    ∀ n, G.maxSubgraphEdgeDistToBipartite n ≤ (n : ℝ).sqrt) ↔ answer(sorry):= by
   sorry
 
 -- TODO(firsching): add the remaining statements/comments
