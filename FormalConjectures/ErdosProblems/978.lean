47d9e68df4323b995bb9875b6d5199a4f7026bfb
Moritz Firsching <firsching@google.com>
2026-03-30 21:50:56 +0200

    feat(ErdosProblems/978): solution for `parts.ii`

--- FormalConjectures/ErdosProblems/978.lean
+++ FormalConjectures/ErdosProblems/978.lean
@@ -58,14 +58,58 @@ theorem erdos_978.variants.sub_two {f : ℤ[X]} (hi : Irreducible f) (hd : 9 ≤
     {n : ℕ | Powerfree (f.natDegree - 2) (f.eval (n : ℤ))}.Infinite := by
   sorry
 
-/-- If $k > 3$ (and $k \neq 2^l$), then are there infinitely many $n$ for which $f(n)$ is
-$(k-2)$-power-free? -/
 @[category research open, AMS 11]
-theorem erdos_978.parts.ii : answer(sorry) ↔ ∀ {f : ℤ[X]}, Irreducible f → f.natDegree > 3 →
+theorem erdos_978.parts.ii : answer(False) ↔ ∀ {f : ℤ[X]}, Irreducible f → f.natDegree > 3 →
     (¬ ∃ l : ℕ, f.natDegree = 2 ^ l) → 0 < f.leadingCoeff →
     (¬ ∃ p : ℕ, p.Prime ∧ ∀ n : ℕ, (p : ℤ) ^ (f.natDegree - 1) ∣ f.eval (n : ℤ)) →
     {n : ℕ | Powerfree (f.natDegree - 2) (f.eval (n : ℤ))}.Infinite := by
-  sorry
+  constructor
+  · intro h
+    exact False.elim h
+  · intro h
+    let f : ℤ[X] := X^6 + 33 * X^5 + 21 * X^4 + 63 * X^3 + 18 * X^2 + 24 * X + 48
+    have h_deg : f.natDegree = 6 := by apply natDegree_add_C.trans (by compute_degree!)
+    have h_deg_gt : f.natDegree > 3 := by omega
+    have h_l : ¬∃ l : ℕ, f.natDegree = 2 ^ l := by use h_deg.symm▸fun⟨A, B⟩=>by norm_num[<-B▸Nat.log_pow _ _] at B
+    have h_lc : 0 < f.leadingCoeff := by norm_num[*,leadingCoeff,f,coeff_X]
+    have h_irred : Irreducible f := by
+      rewrite [ @irreducible_iff]
+      clear h h_l h_lc h_deg_gt
+      aesop
+      · rcases@h_deg▸natDegree_eq_zero_of_isUnit a
+      rw[natDegree_mul]at*
+      · rcases lt_trichotomy @a.natDegree 0 with a | S | S
+        · tauto
+        · exact (.inl (eq_C_of_natDegree_eq_zero S▸isUnit_C.mpr (isUnit_of_dvd_one (((C_dvd_iff_dvd_coeff _ _).mp ⟨b,eq_C_of_natDegree_eq_zero S▸a_1⟩ 6).trans (by ·norm_num[coeff_X])))))
+        rcases lt_trichotomy b.natDegree 0 with a | S|X
+        · cases(a)
+        · exact (.inr (eq_C_of_natDegree_eq_zero S▸isUnit_C.2 (isUnit_of_dvd_one (((C_dvd_iff_dvd_coeff _ _).1 ↑(eq_C_of_natDegree_eq_zero S▸a_1▸dvd_mul_left _ _) (↑6)).trans (by norm_num [coeff_X])))))
+        norm_num[ext_iff]at@a_1
+        have : ∀N ∈ Finset.range 6,3 ∣coeff (a*b) N
+        · norm_num[←a_1,coeff_X,Nat.forall_lt_succ]
+        replace:3 ∣a ∨3 ∣b:=or_iff_not_imp_left.2 fun and=>(C_dvd_iff_dvd_coeff _ _).2 ?_
+        · use absurd ((a_1 _)▸(C_dvd_iff_dvd_coeff _ _).1 (this.elim (·.mul_right b) (·.mul_left a)) 6) (by norm_num[coeff_X])
+        push_cast[coeff_mul,←h_deg, Finset.mem_range] at this⊢
+        conv_rhs at this=>norm_num [ Finset.HasAntidiagonal.antidiagonal,id]
+        conv_rhs at this=>norm_num[ Multiset.Nat.antidiagonal]
+        conv_rhs at this=>norm_num[List.Nat.antidiagonal]
+        refine Nat.strongRec fun A B=>by_contra (and ∘(C_dvd_iff_dvd_coeff _ _).2 ∘Nat.strongRec ∘ fun and R M=>by_contra fun and=>absurd (this (R+A)) ? _)
+        rewrite [Function.comp_def, show (List.sum _)=∑x ∈.range _,_ from rfl,Int.dvd_iff_emod_eq_zero, Finset.sum_int_mod, Finset.sum_eq_single_of_mem R (@ Finset.mem_range_succ_iff.mpr ↑le_self_add)]
+        · use(R.add_sub_cancel_left _)▸mt (· (R.add_lt_add_of_lt_of_le ((le_natDegree_of_ne_zero (and ⟨0,.⟩)).lt_of_ne fun and=>?_) (le_natDegree_of_ne_zero (by valid)))) (and ∘by norm_num[*,Int.prime_three.dvd_mul])
+          use (by norm_num ∘ (@a_1 0▸mul_coeff_zero a b▸mul_dvd_mul (M 0 (and▸S)))) (B 0 (by_contra fun and=>Int.prime_three.not_dvd_mul (by valid) (by apply_rules) (by (fin_omega))))
+        · use fun and R L=>(( L.lt_or_gt.elim (M and ·|>.mul_right _) fun and' =>(B _ (by match@List.mem_range.1 R with | S=>omega)).mul_left _)).modEq_zero_int
+      · refine fun and=>by simp_all
+      · refine fun and=>by simp_all
+    have h_p : ¬∃ p : ℕ, p.Prime ∧ ∀ n : ℕ, (p : ℤ) ^ (f.natDegree - 1) ∣ f.eval (n : ℤ) := by
+      use h_deg.symm▸fun⟨x,A, B⟩=>absurd (B 0) (absurd (B 1) ∘? _)
+      use fun and=>by norm_num[le_antisymm (not_lt.1 fun and' =>absurd ((pow_le_pow_left₀ · (Nat.cast_le.2 and') 5|>.trans (Int.le_of_dvd (by norm_num[f]) and))) (by norm_num):x≤2) A.two_le,f]
+    have h_inf : {n : ℕ | Powerfree (f.natDegree - 2) (f.eval (n : ℤ))}.Infinite := by
+      exact h h_irred h_deg_gt h_l h_lc h_p
+    have h_not_inf : ¬ {n : ℕ | Powerfree (f.natDegree - 2) (f.eval (n : ℤ))}.Infinite := by
+      simp [Powerfree,f]
+      refine h_deg.symm▸BddAbove.finite ⟨64,fun A B=>not_lt.1 fun and=>absurd (@B (2)) ?_⟩
+      use by decide ∘(. ( (ZMod.intCast_zmod_eq_zero_iff_dvd _ _).1 (by zify[(by decide : ∀x:Fin 16,x^6+33*x^5+21*x^4+63*x^3+18*x^2+24*x+48=0)])))
+    exact h_not_inf h_inf
 
 /-- Does `n ^ 4 + 2` represent infinitely many squarefree numbers? -/
 @[category research open, AMS 11]
