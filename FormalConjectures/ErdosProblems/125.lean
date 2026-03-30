e1e26e7239a9e33c76756ce4e0bbdf72ea6fff01
Moritz Firsching <firsching@google.com>
2026-03-30 13:49:14 +0200

    feat(ErdosProblems/125): additional variants (#3665)

--- FormalConjectures/ErdosProblems/125.lean
+++ FormalConjectures/ErdosProblems/125.lean
@@ -20,32 +20,52 @@ import FormalConjectures.Util.ProblemImports
 # Erdős Problem 125
 
 *Reference:* [erdosproblems.com/125](https://www.erdosproblems.com/125)
+
+There are four possibilities for the density of $A+B$:
+1. $A+B$ has zero upper and lower density (and hence also zero density).
+2. $A+B$ has zero lower density, but positive upper density (and hence no density).
+3. $A+B$ has positive upper and lower density that are equal (and hence positive density).
+4. $A+B$ has positive upper and lower density that are unequal (and hence no density).
 -/
 
 open Nat Pointwise
 
 namespace Erdos125
 
-/-
-Let $A = {∑ ε_{k} 3^{k} : ε_{k} ∈ {0,1}}$ be the set of integers which
-have only the digits $0, 1$ when written base 3, and $B = {∑ ε_{k} 4^{k} : ε_{k} ∈ {0,1}}$
-be the set of integers which have only the digits $0, 1$ when written base 4.
-Does $A + B$ have positive density?
+set_option quotPrecheck false
 
-This has been falsified.
+/--
+Let $A$ be the set of integers which have only the digits $0, 1$ when written base 3,
+-/
+local notation "A" => { x : ℕ | (digits 3 x).toFinset ⊆ {0, 1} }
+/--
+and $B$ be the set of integers which have only the digits $0, 1$ when written base 4.
+-/
+local notation "B" => { x : ℕ | (digits 4 x).toFinset ⊆ {0, 1} }
+
+
+/-
+There are four possibilities for the density of $A+B$:
+1. $A+B$ has zero upper and lower density (and hence also zero density).
+2. $A+B$ has zero lower density, but positive upper density (and hence no density).
+3. $A+B$ has positive upper and lower density that are equal (and hence positive density).
+4. $A+B$ has positive upper and lower density that are unequal (and hence no density).
+-/
+
+/--
+Case 3:
+Does $A + B$ have positive upper and lower density that are equal?
+This is the literal interpretation of "positive density" which was falsified.
 -/
 
 @[category research solved, AMS 11,
 formal_proof using formal_conjectures at "https://github.com/google-deepmind/formal-conjectures/blob/300bf771bdbef43d7b9aa2521e633a50fd54dd28/FormalConjectures/ErdosProblems/125.lean"]
 theorem erdos_125 :
-    answer(False) ↔ ({ x : ℕ | (digits 3 x).toFinset ⊆ {0, 1} } +
-      { x : ℕ | (digits 4 x).toFinset ⊆ {0, 1} }).HasPosDensity := by
+    answer(False) ↔ (A + B).HasPosDensity := by
   sorry
 
 /--
-Let $A = {∑ ε_{k} 3^{k} : ε_{k} ∈ {0,1}}$ be the set of integers which
-have only the digits $0, 1$ when written base 3, and $B = {∑ ε_{k} 4^{k} : ε_{k} ∈ {0,1}}$
-be the set of integers which have only the digits $0, 1$ when written base 4.
+Literature question:
 Does $A + B$ have positive lower density?
 
 This has been falsified.
@@ -53,21 +73,44 @@ This has been falsified.
 @[category research solved, AMS 11,
 formal_proof using formal_conjectures at "https://github.com/mo271/formal-conjectures/blob/c27415379b5dbe34105d1fdd707994540c4c6fc7/FormalConjectures/ErdosProblems/125.lean#L468"]
 theorem erdos_125.variants.positive_lower_density :
-    answer(False) ↔ 0 < ({ x : ℕ | (digits 3 x).toFinset ⊆ {0, 1} } +
-      { x : ℕ | (digits 4 x).toFinset ⊆ {0, 1} }).lowerDensity := by
+    answer(False) ↔ 0 < (A + B).lowerDensity := by
   sorry
 
 
 /--
-Let $A = {∑ ε_{k} 3^{k} : ε_{k} ∈ {0,1}}$ be the set of integers which
-have only the digits $0, 1$ when written base 3, and $B = {∑ ε_{k} 4^{k} : ε_{k} ∈ {0,1}}$
-be the set of integers which have only the digits $0, 1$ when written base 4.
+Literature question:
 Does $A + B$ have positive upper density?
 -/
 @[category research open, AMS 11]
 theorem erdos_125.variants.positive_upper_density :
-    answer(sorry) ↔ 0 < ({ x : ℕ | (digits 3 x).toFinset ⊆ {0, 1} } +
-      { x : ℕ | (digits 4 x).toFinset ⊆ {0, 1} }).upperDensity := by
+    answer(sorry) ↔ 0 < (A + B).upperDensity := by
+  sorry
+
+/--
+Case 1:
+Does $A + B$ have zero upper and lower density?
+-/
+@[category research open, AMS 11]
+theorem erdos_125.variants.zero_density :
+    answer(sorry) ↔ (A + B).upperDensity = 0 ∧ (A + B).lowerDensity = 0 := by
+  sorry
+
+/--
+Case 2:
+Does $A + B$ have zero lower density, but positive upper density?
+-/
+@[category research open, AMS 11]
+theorem erdos_125.variants.zero_lower_positive_upper_density :
+    answer(sorry) ↔ (A + B).lowerDensity = 0 ∧ 0 < (A + B).upperDensity := by
+  sorry
+
+/--
+Case 4:
+Does $A + B$ have positive upper and lower density that are unequal?
+-/
+@[category research open, AMS 11]
+theorem erdos_125.variants.positive_unequal_density :
+    answer(sorry) ↔ 0 < (A + B).lowerDensity ∧ (A + B).lowerDensity < (A + B).upperDensity := by
   sorry
 
 end Erdos125
