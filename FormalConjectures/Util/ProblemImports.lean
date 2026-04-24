0cefb4ab3c6a0bdc7032e6bf43c5bee2a5e99fae
Moritz Firsching <firsching@google.com>
2026-04-24 11:23:07 +0200

    feat: fix and enable `ExistsImplicationLinter`

--- FormalConjectures/Util/ProblemImports.lean
+++ FormalConjectures/Util/ProblemImports.lean
@@ -22,8 +22,8 @@ public import FormalConjectures.Util.Linters.AMSLinter
 public import FormalConjectures.Util.Linters.AnswerLinter
 public import FormalConjectures.Util.Linters.CategoryLinter
 public import FormalConjectures.Util.Linters.CopyrightLinter
+public import FormalConjectures.Util.Linters.ExistsImplicationLinter
 public import FormalConjectures.Util.Linters.ModuleDocstringLinter
-
 
 /-!
 # Standard imports for open problems
