6ceeef95a9e8f7e684ef98fb07812ae66eb5d8cc
Moritz Firsching <firsching@google.com>
2026-08-14 11:23:13 +0200

    feat: complete term proofs for 369462

--- FormalConjectures/OEIS/369462.lean
+++ FormalConjectures/OEIS/369462.lean
@@ -54,21 +54,94 @@ noncomputable def a (n : ℕ) : ℕ :=
     0
 
 
-@[category test, AMS 11]
-theorem a_1 : a 1 = 0 := by
-  sorry
+
+lemma a_eq_0_of_N_le_50 {n : ℕ} (hn1 : 1 ≤ n) (hn2 : 12 * n - 1 ≤ 50) : a n = 0 := by
+  unfold a
+  rw [if_pos hn1]
+  dsimp
+  have h_empty : (Finset.filter (fun t : ℕ × ℕ × ℕ =>
+      t.1.Prime ∧ t.1 ≠ 2 ∧ t.2.1.Prime ∧ t.2.1 ≠ 2 ∧ t.2.2.Prime ∧ t.2.2 ≠ 2 ∧
+      t.1 ≤ t.2.1 ∧ t.2.1 ≤ t.2.2 ∧ t.1 * t.2.1 + t.1 * t.2.2 + t.2.1 * t.2.2 = 12 * n - 1)
+    ((Finset.range (12 * n - 1 + 1)) ×ˢ ((Finset.range (12 * n - 1 + 1)) ×ˢ (Finset.range (12 * n - 1 + 1))))) = ∅ := by
+    rw [Finset.filter_eq_empty_iff]
+    rintro ⟨p, q, r⟩ _
+    simp only [not_and]
+    intro hp hp2 hq hq2 hr hr2 hpq hqr
+    have hp3 : 3 ≤ p := by
+      cases p with
+      | zero => exact False.elim (Nat.not_prime_zero hp)
+      | succ p' =>
+        cases p' with
+        | zero => exact False.elim (Nat.not_prime_one hp)
+        | succ p'' =>
+          cases p'' with
+          | zero => exact False.elim (hp2 rfl)
+          | succ p''' => omega
+    have hq3 : 3 ≤ q := by omega
+    have hr3 : 3 ≤ r := by omega
+    have hN : 12 * n - 1 ≤ 47 := by omega
+    intro heq
+    have : p * q + p * r + q * r ≤ 47 := heq.symm ▸ hN
+    have hr7 : r < 7 := by
+      by_contra!
+      nlinarith
+    have h_r : r = 3 ∨ r = 5 := by
+      have : r ≤ 6 := by omega
+      interval_cases r
+      · left; rfl
+      · exact False.elim (by revert hr; decide)
+      · right; rfl
+      · exact False.elim (by revert hr; decide)
+    have h_q : q = 3 ∨ q = 5 := by
+      have : q ≤ 5 := by omega
+      interval_cases q
+      · left; rfl
+      · exact False.elim (by revert hq; decide)
+      · right; rfl
+    have h_p : p = 3 ∨ p = 5 := by
+      have : p ≤ 5 := by omega
+      interval_cases p
+      · left; rfl
+      · exact False.elim (by revert hp; decide)
+      · right; rfl
+    rcases h_p with rfl | rfl
+    · rcases h_q with rfl | rfl
+      · rcases h_r with rfl | rfl
+        · revert heq; norm_num; intro heq; omega
+        · revert heq; norm_num; intro heq; omega
+      · rcases h_r with rfl | rfl
+        · revert hqr; norm_num
+        · revert heq; norm_num; intro heq; omega
+    · rcases h_q with rfl | rfl
+      · rcases h_r with rfl | rfl
+        · revert hpq; norm_num
+        · revert hpq; norm_num
+      · rcases h_r with rfl | rfl
+        · revert hqr; norm_num
+        · revert heq; norm_num; intro heq; omega
+  have h_eq : Finset.filter (fun t =>
+        Nat.Prime t.1 ∧
+          ¬t.1 = 2 ∧
+            Nat.Prime t.2.1 ∧
+              ¬t.2.1 = 2 ∧
+                Nat.Prime t.2.2 ∧
+                  ¬t.2.2 = 2 ∧ t.1 ≤ t.2.1 ∧ t.2.1 ≤ t.2.2 ∧ t.1 * t.2.1 + t.1 * t.2.2 + t.2.1 * t.2.2 = 12 * n - 1)
+      (Finset.range (12 * n - 1 + 1) ×ˢ Finset.range (12 * n - 1 + 1) ×ˢ Finset.range (12 * n - 1 + 1)) = ∅ := h_empty
+  rw [h_eq]
+  exact rfl
 
 @[category test, AMS 11]
-theorem a_2 : a 2 = 0 := by
-  sorry
+theorem a_1 : a 1 = 0 := a_eq_0_of_N_le_50 (by norm_num) (by norm_num)
 
 @[category test, AMS 11]
-theorem a_3 : a 3 = 0 := by
-  sorry
+theorem a_2 : a 2 = 0 := a_eq_0_of_N_le_50 (by norm_num) (by norm_num)
 
 @[category test, AMS 11]
-theorem a_4 : a 4 = 0 := by
-  sorry
+theorem a_3 : a 3 = 0 := a_eq_0_of_N_le_50 (by norm_num) (by norm_num)
+
+@[category test, AMS 11]
+theorem a_4 : a 4 = 0 := a_eq_0_of_N_le_50 (by norm_num) (by norm_num)
+
 
 /--
 Conjecture a: Is there only a finite number of 0's in this sequence?
