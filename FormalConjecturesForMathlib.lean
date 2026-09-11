5e9c65dd7e5e3b5282254b53fc736d526a877792
William Coram <williamecoram@gmail.com>
2026-09-11 09:32:42 +0000

    feat: real and complex periods (short file) (#5534)
    This is a short file that only gives the definition of real and complex
    periods of an elliptic curve. This has no API - for the real one see
    FC#5370, for the complex one I have not made a PR - let me know if I
    should.
    
    ---------
    
    Co-authored-by: Moritz Firsching <firsching@google.com>

--- FormalConjecturesForMathlib.lean
+++ FormalConjecturesForMathlib.lean
@@ -27,6 +27,7 @@ public import FormalConjecturesForMathlib.Algebra.Polynomial.Basic
 public import FormalConjecturesForMathlib.Algebra.Polynomial.HasseDeriv
 public import FormalConjecturesForMathlib.Algebra.Powerfree
 public import FormalConjecturesForMathlib.Algebra.QuadraticAlgebra.Instances
+public import FormalConjecturesForMathlib.AlgebraicGeometry.EllipticCurve.GlobalPeriod
 public import FormalConjecturesForMathlib.AlgebraicGeometry.ProjectiveSpace
 public import FormalConjecturesForMathlib.AlgebraicGeometry.VectorBundle
 public import FormalConjecturesForMathlib.Analysis.Asymptotics.Basic
